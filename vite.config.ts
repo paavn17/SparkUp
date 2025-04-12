10112ead2171fd7d25ea11554843d6848a557897
Pavan <pavandamarouthu@gmail.com>
2025-04-12 12:25:51 +0530

    final update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
+  base: '/', // <- ✅ base goes here
   plugins: [
-    tailwindcss(),
-  ],
+    react(),
+    tailwindcss()
+  ]
 })
